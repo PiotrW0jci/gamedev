88f77338d4119f2933f61d2f453c79992b00ef08
Piotr Wójcik <p.w9714x3@gmail.com>
2019-05-15 23:23:22 +0200

    Update Game.java

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -6,9 +6,11 @@ import java.awt.event.ActionListener;
 public class Game {
 
     ChoiceHandler chHandler= new ChoiceHandler();
-    TitleScreenHandler tsHandler= new TitleScreenHandler();
     UI ui = new UI();
     VisibilityManager vm = new VisibilityManager(ui);
+    Story story = new Story(this,ui,vm);
+
+    String nextPosition1,nextPosition2,nextPosition3,nextPosition4;
 
 
 
@@ -19,58 +21,25 @@ public class Game {
 
     public Game() {
 
-        ui.createUI(chHandler,tsHandler);
+        ui.createUI(chHandler);
+        story.defaultSetup();
         vm.showTitleScreen();
 
     }
 
 
-    public void playerSetup(){
-        Points=0;
-        etcsPointsNumber.setText(""+Points);
-        umcs();
-    }
-    public void umcs(){
-        Position="umcs";
-        mainTextArea.setText("Witaj Vladka czas ntwoją fabule");
 
-        choice1.setText("wybierz wolnosc uciekaj");
-        choice2.setText("wejdz");
-        choice3.setText("3");
-        choice4.setText("");
-    }
-
-    public void run(){
-        Position="run";
-        mainTextArea.setText("ciesz się wolnoscią");
-        choice1.setText("wroc");
-        choice2.setText("");
-        choice3.setText("");
-        choice4.setText("");
-    }
-    public class  TitleScreenHandler  implements  ActionListener{
-        public void actionPerformed(ActionEvent event) {
-            creteGameScreen();
-
-        }
-    }
 
     public class ChoiceHandler implements  ActionListener{
         public void actionPerformed(ActionEvent event){
             String yourChoice= event.getActionCommand();
 
-            switch (Position){
-                case "umcs":
-                    switch(yourChoice){
-                        case "c1" :run();
-                        case "c2": break;
-                        case "c3": break;
-                        case "c4": break;
-                    }
-                    break;
-                case "run":
-                    switch (yourChoice){
-                        case "c1":umcs();
+            switch (yourChoice){
+                        case "start": vm.titleToUmcs (); story.umcs();break;
+                        case "c1" :story.selectPosition(nextPosition1); break;
+                        case "c2": story.selectPosition(nextPosition2);break;
+                        case "c3": story.selectPosition(nextPosition3);break;
+                        case "c4": story.selectPosition(nextPosition4);break;
                     }
 
 
@@ -79,4 +48,3 @@ public class Game {
         }
     }
 
-}
